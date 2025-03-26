54f1ddb0926296c9e6228e8aa56a6f9586cb9a26
Andrey Hangan <NeoGame555@inbox.ru>
2025-03-26 20:58:25 +0300

    weather app

---
+++ src/components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Weather() {
+  return (
+    <div className='weather_container'>
+        <div className='main_section'></div>
+        <div className='side_section'></div>
+    </div>
+  )
+}
+
+export default Weather
