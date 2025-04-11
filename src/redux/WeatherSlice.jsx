d3812c6a8ea5fef7693cb2170328eae7ed3bb3fa
Andrey Hangan <NeoGame555@inbox.ru>
2025-04-12 00:16:44 +0300

    add weatherSlice

--- src/redux/WeatherSlice.jsx
+++ src/redux/WeatherSlice.jsx
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 const API_KEY = '9310a0427d494372b03180547250404'
@@ -9,5 +9,23 @@ export const fetchForecastByCity = createAsyncThunk(
     'weather/fetchForecastByCity',
     async(city) => {
         const response = await axios.get(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=5`)
+        return response.data
     }
 )
+
+const weatherSlice = createSlice({
+    name: 'weather',
+    initialState: {
+        forecast: {},
+        status: 'idle',
+        error: 'null'
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchForecastByCity, (state, action) => {
+            state.forecast = action.payload
+        })
+    }
+});
+
+export default weatherSlice.reducer;
